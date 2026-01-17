690d63a1ad9908f737ccf6d479afe3d76b05f7d5
WaxyCraft <waxy1221@gmail.com>
2026-01-17 12:28:43 -0600

    it did not commit robotmap for some reason idk

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+import com.ctre.phoenix6.CANBus;
+
+public class RobotMap {
+    public static final int TURRET_MOTOR_CAN_ID = Constants.PLACEHOLDER_INT; 
+
+    public static final CANBus RIO_CAN_BUS = new CANBus("rio"); 
+}
